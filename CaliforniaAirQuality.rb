e27479c9836c6bfefc8658a043c605f4c117c12a
Daniel Miessler <daniel@danielmiessler.com>
2015-01-11 23:41:14 -0800

    Fixed zip code.

--- CaliforniaAirQuality.rb
+++ CaliforniaAirQuality.rb
@@ -2,10 +2,7 @@ require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
 
-page = Nokogiri::HTML(open("http://www.airnow.gov/?action=airnow.local_city&zipcode=31904&submit=Go"))
+page = Nokogiri::HTML(open("http://www.airnow.gov/?action=airnow.local_city&zipcode=94102&submit=Go"))
 
 # Get the current air quality
 puts page.css('table.AQData td.AQDataPollDetails a').text
-
-
-

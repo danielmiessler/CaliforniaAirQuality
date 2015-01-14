5c5602714c6fa2d16d558de7c91a57a7dc1b0cba
Daniel Miessler <daniel@danielmiessler.com>
2015-01-13 16:02:22 -0800

    Fixed extraction.

--- CaliforniaAirQuality.rb
+++ CaliforniaAirQuality.rb
@@ -6,4 +6,4 @@ require 'open-uri'
 page = Nokogiri::HTML(open("http://www.airnow.gov/?action=airnow.local_city&zipcode=94102&submit=Go"))
 
 # Get the current air quality
-puts page.css('table.AQData td.AQDataPollDetails a').text
+puts page.css('table.AQData td.AQDataLg').text

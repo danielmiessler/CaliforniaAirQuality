e23b81f578d832a430ad9eaeac857fe72777f1ce
removedporn <86824510+removedporn@users.noreply.github.com>
2022-05-02 02:53:19 +0800

    Delete CaliforniaAirQuality.rb

--- CaliforniaAirQuality.rb
+++
@@ -1,9 +0,0 @@
-require 'rubygems'
-require 'nokogiri'
-require 'open-uri'
-
-# Change for your ZIP
-page = Nokogiri::HTML(open("http://www.airnow.gov/?action=airnow.local_city&zipcode=94102&submit=Go"))
-
-# Get the current air quality
-puts page.css('table.AQData td.AQDataLg').text
